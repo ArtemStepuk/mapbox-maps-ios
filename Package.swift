10965ab3a312e93121e83062d50c0c55f443573d
Artem Stepuk <artem.stepuk@mapbox.com>
2026-04-16 16:47:29 +0300

    11.23.0-SNAPSHOT-04-13--12-46.git-450b54c

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.23.0-SNAPSHOT-04-16--02-05.git-8729722"
-let coreMapsVersion: Version = "11.23.0-SNAPSHOT-04-16--02-05.git-8729722"
+let commonVersion: Version = "24.23.0-SNAPSHOT-04-13--12-46.git-450b54c"
+let coreMapsVersion: Version = "11.23.0-SNAPSHOT-04-13--12-46.git-450b54c"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil
